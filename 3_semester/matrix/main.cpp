c83a3aca30edaad59fb6cf45e170d2544dc4fd93
aminjonshermatov <aminjonshermatov@gmail.com>
2021-10-07 22:36:57 +0300

    refactoring matrix

--- 3_semester/matrix/main.cpp
+++ 3_semester/matrix/main.cpp
@@ -2,19 +2,6 @@
 // Created by aminjonshermatov on 10/6/2021.
 //
 
-/**
- * класс матрица n*n
- * +, -, *, /
- * +=, -=, *=, /=
- * ==, !=
- * obratnaya, transponirovaniye, vozvedenie v stepen
- * opredelitel, norma
- *
- * types: kvadrat, diagonal, nullable, simmetry, nozhniy treugolniy
- *
- * <<, >>
- * */
-
 #include "Matrix.h"
 
 int main() {
