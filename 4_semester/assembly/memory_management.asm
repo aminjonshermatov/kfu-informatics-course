984ef43b198f4aa63d1aa524f3c1abfdec74811d
aminjonshermatov <aminjonshermatov@gmail.com>
2022-05-10 01:04:26 +0300

    latex presentation

---
+++ 4_semester/assembly/memory_management.asm
@@ -0,0 +1,34 @@
+section .data
+	msg db 'Allocated 16kb of memory', 0xA
+	len equ $-msg
+
+section .text
+	global _start
+_start:
+	mov eax, 45
+	xor ebx, ebx
+	int 80h
+
+	add eax, 16 * 1024
+	mov ebx, eax
+	mov eax, 45
+	int 80h
+
+	cmp eax, 0
+	jl exit_
+	mov edi, eax
+	sub edi, 4
+	mov ecx, 4 * 1024
+	xor eax, eax
+	std
+	rep stosd
+	cld
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 80h
+exit_:
+	mov eax, 1
+	int 80h
